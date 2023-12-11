f11b239eb25caff7bbf329a6a6b4f72fae64222a
RomanK11 <romankorovin032@gmail.com>
2023-12-11 22:13:09 +0300

    Добавлена "полноценная" домашняя страница

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,38 +1,399 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
-import 'package:laps/model/user_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:laps/pages/currents/current_user.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
-  _HomePage createState() => _HomePage();
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
 }
 
-class _HomePage extends State {
-  String email = '';
+class _MyBottomNavigationBarState extends State<HomePage> {
+  int _selectedIndex = 0;
 
+  List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Page2(),
+    Page3(),
+    Page4(),
+    Page5(),
+    Page6(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0x4CFFC673),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 255, 199, 115),
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pets_outlined),
+            label: 'Page 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_walk),
+            label: 'Page 3',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Page 4',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Page 5',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Аккаунт',
+          ),
+        ],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+// Placeholder pages for demonstration
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Container(
           width: size.width,
           height: size.height,
-          padding: EdgeInsets.only(top: 30, left: 25, right: 20),
+          padding: EdgeInsets.only(top: 15, left: 10, right: 10),
           color: Color(0x4CFFC673),
-          child: FutureBuilder<String>(
-            future: getTextFromPreferences(),
-            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.toString());
-              }
-              return CircularProgressIndicator();
-            },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Четыре лапы',
+                  style: TextStyle(
+                    color: Color(0xFF0261BC),
+                    fontSize: 36,
+                    fontFamily: 'Comforter',
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.54,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    Icon(Icons.favorite), // иконка
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Мои питомцы',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w500,
+                            height: 0.08,
+                            letterSpacing: 0.10,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          alignment: Alignment.centerLeft,
+                          minimumSize: Size(304, 48),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.account_box), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Аккаунт',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Opacity(
+                opacity: 0.12,
+                child: Container(
+                  width: 304,
+                  height: 1,
+                  decoration: BoxDecoration(color: Color(0xFF002033)),
+                ),
+              ),
+              SizedBox(
+                height: 11,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 12, left: 16),
+                child: Row(
+                  children: [
+                    Text(
+                      'События и уведомления',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w200,
+                        height: 0.10,
+                        letterSpacing: 0.10,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.pets), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'План прогулок',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.message), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Сообщения',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.notification_important), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Уведомления',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Opacity(
+                opacity: 0.12,
+                child: Container(
+                  width: 304,
+                  height: 1,
+                  decoration: BoxDecoration(color: Color(0xFF002033)),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 12, left: 16),
+                child: Row(
+                  children: [
+                    Text(
+                      'Дополнительно',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w200,
+                        height: 0.10,
+                        letterSpacing: 0.10,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.edit_document), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Политика конфиденциальности',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.document_scanner), // иконка
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Пользовательское соглашение',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0.08,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        minimumSize: Size(304, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -40,8 +401,53 @@ class _HomePage extends State {
   }
 }
 
-Future<String> getTextFromPreferences() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String text = prefs.getString('emailKey').toString();
-  return text ?? '';
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 2'),
+    );
+  }
 }
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 3'),
+    );
+  }
+}
+
+class Page4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 4'),
+    );
+  }
+}
+
+class Page5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 5'),
+    );
+  }
+}
+
+class Page6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Аккаунт'),
+    );
+  }
+}
+
+// Future<String> getTextFromPreferences() async {
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   String text = prefs.getString('emailKey').toString();
+//   return text ?? '';
+// }
